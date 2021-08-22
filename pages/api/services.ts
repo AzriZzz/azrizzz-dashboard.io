8b295de4d5fdea0d6cb588d38271152c687dffbc
Muhammad Azri <lenovorosak@gmail.com>
2021-08-23 01:08:05 +0800

    Add function name to the services

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import {services} from'../../data'
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
+export default function Service(req: NextApiRequest, res: NextApiResponse) {
   // by default get request services
   res.status(200).json({services});
 };
