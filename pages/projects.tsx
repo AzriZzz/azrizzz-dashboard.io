b5b158cfae96a7302b2c65b5ab7dec35143a8f04
Muhammad Azri <lenovorosak@gmail.com>
2021-08-26 23:12:22 +0800

    add active state for filter category

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,29 +7,33 @@ import { Category } from "../type";
 function Projects() {
   const [projects, setProjects] = useState(projectsData);
 
-  const [active, setActive] = useState('all');
+  const [active, setActive] = useState("all");
 
-
-  const handlerFilterCategory = (category: Category | 'all') => {
-    if(category === 'all') {
+  const handlerFilterCategory = (category: Category | "all") => {
+    if (category === "all") {
       setProjects(projectsData);
-      setActive(category)
-      return
+      setActive(category);
+      return;
     }
 
-    const newArray = projectsData?.filter( project => project.category.includes(category));
+    const newArray = projectsData?.filter((project) =>
+      project.category.includes(category)
+    );
     setProjects(newArray);
-    setActive(category)
-  }
+    setActive(category);
+  };
 
   return (
     // overflow-y-scroll + active only the more than 6 project available
-    <div className="px-5 py-2 " style={{ height: '65vh'}}>
-      <ProjectsNavbar handlerFilterCategory={handlerFilterCategory} active={active} />
+    <div className="px-5 py-2" style={{ height: "65vh" }}>
+      <ProjectsNavbar
+        handlerFilterCategory={handlerFilterCategory}
+        active={active}
+      />
 
       <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
-            <ProjectCard project={project} key={project.name} />
+          <ProjectCard project={project} key={project.name} />
         ))}
       </div>
     </div>
