0267204fe7e208eb0a123fb14833c95c51445213
Muhammad Azri <lenovorosak@gmail.com>
2021-08-26 22:00:29 +0800

    Add placeholder for projects

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,17 @@ import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 
 import { ThemeProvider } from 'next-themes'
-
+// shadow-custom-light
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute='class'>
       <div className="grid grid-cols-12 gap-6 px-5 lg:mb-0 md:mb-16 lg:px-36 xl:px-48 my-14 sm:px-20 md:px-32">
-        <div className="h-full col-span-12 p-4 text-base text-center bg-white rounded-lg dark:bg-dark-500 lg:col-span-3 shadow-custom-light dark:shadow-custom-dark">
+
+        <div className="h-full col-span-12 p-4 text-base text-center transition duration-150 ease-in-out bg-white rounded-lg bg-opacity-70 bg-clip-padding dark:bg-dark-500 lg:col-span-3 dark:shadow-custom-dark hover:shadow-custom-light" style={{backdropFilter: 'blur(20px)'}}>
           <Sidebar  />
         </div>
-        <div className="flex flex-col col-span-12 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-dark-500 lg:col-span-9 shadow-custom-light dark:shadow-custom-dark">
+
+        <div className="flex flex-col col-span-12 overflow-hidden bg-white rounded-lg shadow-lg bg-clip-padding bg-opacity-70 dark:bg-dark-500 lg:col-span-9 dark:shadow-custom-dark" style={{backdropFilter: 'blur(20px)'}}>
           <Navbar />
           <Component {...pageProps} />
         </div>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,10 +3,11 @@ import { projects } from "../data";
 
 function Projects() {
   return (
-    <div className="p-4">
+    // overflow-y-scroll + active only the more than 6 project available
+    <div className="px-5 py-2 " style={{ height: '65vh'}}>
       <nav>Navbar</nav>
 
-      <div className="grid grid-cols-12 gap-4 my-3">
+      <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
             <ProjectCard project={project} key={project.name} />
         ))}
