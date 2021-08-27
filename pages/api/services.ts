df3240f3df225360d5a0f3d76e0502525fad343d
Muhammad Azri <lenovorosak@gmail.com>
2021-08-27 23:10:38 +0800

    fix arrow function in services

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { services } from "../../data";
 
-export default (req:NextApiRequest, res:NextApiResponse) => {
+export  default function Services(req:NextApiRequest, res:NextApiResponse){
   // by default get request services
   res.status(200).json({ services });
 };
