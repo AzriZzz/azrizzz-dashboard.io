5ce8e2adb64eee501b8619159f25fc575882944e
Muhammad Azri <lenovorosak@gmail.com>
2021-08-28 07:13:33 +0800

    Fixed the breaking part where tailwind not applied in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.{tsx}", "./components/**/*.{tsx}"],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
